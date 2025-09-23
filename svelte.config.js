b4c6cff390e50070e6a49d334e2a9870da71c838
komeil <catinrage@gmail.com>
2025-09-23 14:45:56 +0330

    feat: Add Melt UI preprocessor to Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { preprocessMeltUI } from '@melt-ui/pp';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from 'svelte-adapter-bun';
 
@@ -5,7 +6,7 @@ import adapter from 'svelte-adapter-bun';
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), preprocessMeltUI()],
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
