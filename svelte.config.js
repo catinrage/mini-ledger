71112cfef62947371e0738abd7d43e9d32aabf43
komeil <catinrage@gmail.com>
2026-05-19 15:04:06 +0330

    Fix SvelteKit production build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { preprocessMeltUI } from '@melt-ui/pp';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from 'svelte-adapter-bun';
 
@@ -6,7 +6,7 @@ import adapter from 'svelte-adapter-bun';
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), preprocessMeltUI()],
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -20,7 +20,9 @@ const config = {
       $components: './src/lib/ui/components',
       '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
     },
-    csrf: false,
+    csrf: {
+      trustedOrigins: ['*'],
+    },
   },
 };
 export default config;
